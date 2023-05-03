aa93b920e6e8911d04f5a9040c6af842a5a617c3
helloashik17 <asiknoman150@gmail.com>
2023-05-03 17:06:19 +0600

    terms and condition page designed

---
+++ src/components/Terms/Terms.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Terms = () => {
+    return (
+        <Container>
+            <h2>Terms and Conditions</h2>
+            <p>Welcome to Kitchen Concept. These terms and conditions outline the rules and regulations for the use of our website.</p>
+            <h3>Acceptance of Terms</h3>
+            <p>By accessing or using the website, you agree to be bound by these terms and conditions. If you do not agree to these terms and conditions, please do not use the website.</p>
+            <h3>Use of the Website</h3>
+            <p>You must be at least 18 years old to use our website. You agree to use the website only for lawful purposes and in accordance with these terms and conditions. You are prohibited from interfering with the proper functioning of the website.</p>
+            <h3>Intellectual Property</h3>
+            <p>The content on our website, including but not limited to text, images, graphics, logos, and code, is the property of Kitchen Concept or its licensors and is protected by copyright and other intellectual property laws. You may not copy, distribute, modify, or create derivative works based on this content without our prior written consent.</p>
+            <h3>Disclaimer</h3>
+            <p>Our website is provided on an "as is" and "as available" basis. We make no representations or warranties of any kind, express or implied, as to the operation of the website or the information, content, materials, or products included on the website. You use the website at your own risk.</p>
+            <h3>Limitation of Liability</h3>
+            <p>In no event shall Kitchen Concept be liable for any damages, including but not limited to direct, indirect, incidental, punitive, and consequential damages, arising from the use of or inability to use the website.</p>
+            <h3>Modification of Terms</h3>
+            <p>We reserve the right to modify these terms and conditions at any time. You should review these terms and conditions regularly. Your continued use of the website after any modifications indicates your acceptance of the modified terms and conditions.</p>
+            <h3>Contact Us</h3>
+            <p>If you have any questions about these terms and conditions, please contact us at info@kitchenconcept.com.</p>
+        </Container>
+    );
+};
+
+export default Terms;
